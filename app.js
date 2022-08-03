2480f3bed51c5b758cf90d0602b2410d99685f21
auliamarsya <34575662+auliamarsya@users.noreply.github.com>
2022-08-03 18:19:21 +0700

    update base_url

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 // import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/db_staycation');
+mongoose.connect('mongodb+srv://auliamarsya:auliamarsya@cluster0.evvrl.mongodb.net/db_staycation?retryWrites=true&w=majority');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- seed.js
+++ seed.js
@@ -2,7 +2,7 @@ var seeder = require('mongoose-seed');
 var mongoose = require('mongoose');
 
 // Connect to MongoDB via Mongoose
-seeder.connect('mongodb://localhost:27017/db_staycation', {} , function () {
+seeder.connect('mongodb+srv://auliamarsya:auliamarsya@cluster0.evvrl.mongodb.net/db_staycation?retryWrites=true&w=majority', {} , function () {
 
   // Load Mongoose models
   seeder.loadModels([
